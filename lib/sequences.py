a145edff8f2e5d54ef2ab7c9b939cf4903c147f1
Thomas <thomasrains01@gmail.com>
2025-01-18 13:16:23 -0800

    submits fib test

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+    new_list = []
+    fib_list = []
+    for n in range(length):
+        new_list.append(n)
+    if length < 3:
+        print(new_list)
+    elif length >= 3:
+        for i in new_list:
+            if i < 2:
+                fib_list.append(i)
+            elif i >= 2:
+                fib_list.append((fib_list[i-1])+(fib_list[i-2]))
+        print(fib_list)
+
+
+
+
+            
